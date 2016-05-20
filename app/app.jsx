211aa2cc1234aecf3d24ffbc4eb7bb802f233fc4
Kirkhammetz <Neofrascati@hotmail.it>
2016-05-20 11:42:17 +0200

    init

---
+++ app/app.jsx
@@ -0,0 +1,19 @@
+/** REACT **/
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Route, Router, IndexRoute, hashHistory } from 'react-router'
+
+/** COMPONENTS **/
+
+
+// Foundation Styles
+$(document).foundation();
+
+// App Styles
+require('style!css!sass!applicationStyles');
+
+
+ReactDOM.render(
+  <p>React Boilerplate</p>
+  ,document.getElementById('app')
+);
